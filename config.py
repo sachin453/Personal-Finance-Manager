84154b26fe1ef8e0ceed73d2f6e3b8e4064b9fc2
Sachin Kuntal <107995067+sachin453@users.noreply.github.com>
2025-09-11 15:19:32 +0530

    removing config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ["GEMINI_API_KEY"]        = "AIzaSyCd50QIp_00OBuFadlKh4-r2jzQKaRvRC8"
-os.environ["HF_HOME"]               = "./cache/"
-os.environ["TRANSFORMERS_CACHE"]    = "./cache/"
-os.environ["TORCH_HOME"]            = "./cache/"
